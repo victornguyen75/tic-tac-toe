02bd41d3d4ef7f4c2d9a94e7d7236ba5cb1eb0d8
Nguyen, Victor <vdn140030@gmail.com>
2020-12-21 16:49:16 -0600

    feat(index.js): add state properties

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square />
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
