4deb1b3fc61b8a5c7259d834ac719e12dfe244a1
Nguyen, Victor <vdn140030@gmail.com>
2020-12-21 19:53:49 -0600

    fix(index.js): store the position of i directly in setState

--- src/index.js
+++ src/index.js
@@ -60,7 +60,6 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    const position = i;
 
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -70,7 +69,7 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
-        position,
+        position: i
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
