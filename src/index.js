9588edc8be1714c9a7b99028c7e60cce1cff0aee
Nguyen, Victor <vdn140030@gmail.com>
2020-12-21 18:27:31 -0600

    fix(index.js): import styles; fix typo for jumpTo method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function Square(props) {
   return (
@@ -74,7 +75,7 @@ class Game extends React.Component {
     });
   }
 
-  jumptTo(step) {
+  jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2) === 0,
